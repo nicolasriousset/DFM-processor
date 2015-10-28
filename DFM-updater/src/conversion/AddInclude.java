2f6c86f689c166af4b8b5dcf5c69d57d52a4bd1c
Nicolas <nicolas@riousset.com>
2015-10-28 17:39:00 +0100

    New condition for the AddInclude rule

--- DFM-updater/src/conversion/AddInclude.java
+++ DFM-updater/src/conversion/AddInclude.java
@@ -1,5 +1,6 @@
 package conversion;
 
+import conversion.condition.IConversionCondition;
 import cpp.CppClass;
 import cpp.CppClass.CppFile;
 import dfm.DfmObject;
@@ -7,14 +8,25 @@ import dfm.DfmObject;
 public class AddInclude extends AConversionRule {
     String newHeader;
     CppFile dest;
+    IConversionCondition condition;
     
     public AddInclude(CppFile aDest, String aNewHeader) {
         newHeader = aNewHeader;
         dest = aDest;
     }
     
+    public AddInclude(CppFile aDest, String aNewHeader, IConversionCondition aCondition) {
+        newHeader = aNewHeader;
+        dest = aDest;
+        condition = aCondition;
+    }
+    
+    
     @Override
     public boolean isApplicable(DfmObject dfmObject, CppClass cppClass) {
+    	if (condition != null && !condition.isVerified(dfmObject, cppClass))
+    		return false;
+    	
         return true;
     }
 
